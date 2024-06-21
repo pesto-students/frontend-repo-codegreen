f853bd1ddbd69e408a13f9101bae9173ae8b96a8
Sanika <sanika@theamberhorizons.com>
2024-06-21 17:49:47 +0530

    added mobile menu and logic for show/hide based on user login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import styles from './Navbar.module.css'
-import webLogo from '../../assets/images/logo with side text.svg';
-import HamburgerMenu from './HamburgerMenu/HamburgerMenu'
-import MenuItems from './MenuItems/MenuItems';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import styles from "./Navbar.module.css";
+import webLogo from "../../assets/images/logo with side text.svg";
+import HamburgerMenu from "./HamburgerMenu/HamburgerMenu";
+import MenuItems from "./MenuItems/MenuItems";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header>
-      <NavLink to='/' className={styles.homeLink}>
-      <img id={styles.logo} src={webLogo} alt="GreenGrow Logo" />
-        </NavLink>
-     
-      <HamburgerMenu>
+    <header className="flex flex-row flex-wrap justify-between items-center bg-transparent border-b-2 border-darkest-green pb-[30px]">
+      <NavLink to="/" className="w-[65%] md:w-[50%] ">
+        <img
+          id={styles.logo}
+          src={webLogo}
+          alt="GreenGrow Logo"
+          className="max-w-[200px]"
+        />
+      </NavLink>
+
+      <HamburgerMenu />
+
+      <div
+        id={styles.desktopMenu}
+        className="hidden md:flex justify-center w-[50%]"
+      >
         <MenuItems />
-         </HamburgerMenu> 
-        <div id={styles.desktopMenu}>
-          <MenuItems />
-        </div>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Navbar;
